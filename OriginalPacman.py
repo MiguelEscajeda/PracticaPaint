0fb180027412d474ff76223d83923e4c5c4d88d8
ErnestoFlores09 <83657835+ErnestoFlores09@users.noreply.github.com>
2021-05-06 16:56:26 -0500

    Modifiqué el tablero
    Modifiqué estructura y color de tablero, tamaño de galletas, velocidad de juego y que mostrara el score.

--- OriginalPacman.py
+++ OriginalPacman.py
@@ -16,6 +16,8 @@ state = {'score': 0}
 # Hace invisible la turtle-- creando dos objetos de clase turtle
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
+info = Turtle(visible=False)
+tab = Turtle(visible=False)
 
 # Direccion del Pacman
 aim = vector(5, 0)
@@ -38,19 +40,19 @@ ghosts = [
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
     0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
     0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
     0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
-    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
     0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
@@ -101,7 +103,7 @@ def valid(point):
 def world():
     "Draw world using path."
     bgcolor('black')
-    path.color('blue')
+    path.color('purple1')
     
     # Recorre toda la lista (tiles)
     for index in range(len(tiles)):
@@ -118,15 +120,30 @@ def world():
             if tile == 1:
                 path.up()
                 path.goto(x + 10, y + 10)
-                path.dot(2, 'white')
+                path.dot(4, 'white')
 
 def move():
+    #muestra nuestro nombres
+    def info_alumno1():
+        info.up()
+        info.goto(0,190)
+        info.color('blue')
+        info.write('ErnestoFloresA00828975', align='left', font=('Arial',12,'normal'))
+        info.up()
+        info.goto(-200,190)
+        info.color('white')
+        info.write('MiguelEscajedaA008293', align='left', font=('Arial',12,'normal'))
+    info_alumno1()
     # Lista de colores paraz los fantasmas
     colores = ['red', 'green', 'pink', 'white']
     "Move pacman and all ghosts."
-    writer.undo()
+    
+    # muestra el score
+    tab.undo()
+    tab.goto(150,150)
+    tab.color('white')
     valor = state['score']
-    writer.write(f'Score:{valor}')
+    tab.write(f'Score:{valor}')
 
     # Limpia la ventana
     clear()
@@ -195,7 +212,7 @@ def move():
             writer.write('GAME OVER', font=('Arial',30,'normal'))
             return
     # vuelve a llmar  a la funcion dentro e 100 milisegundos
-    ontimer(move, 100)
+    ontimer(move, 20)
 
 def change(x, y):
     "Change pacman aim if valid."
@@ -213,12 +230,6 @@ hideturtle()
 
 # Oculta toda forma de dibujar
 tracer(False)
-
-# mueve la turtle a la poscion 160,160
-writer.goto(160, 160)
-writer.color('white')
-valor = state['score']
-writer.write(f'Score:{valor}')
 
 # Activar, escuchar los eventos del teclado
 listen()
